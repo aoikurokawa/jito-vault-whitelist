bccdef4e0dfb97d6e535d3d1430220a40e4895b3
aoikurokawa <ht.so.ik0102@gmail.com>
2025-03-17 15:48:50 +0900

    fix: update

--- core/src/discriminator.rs
+++ core/src/discriminator.rs
@@ -4,10 +4,10 @@ use crate::config::Config;
 
 /// Discriminators for HelloWorldNcn accounts
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum HelloWorldNcnDiscriminator {
+pub enum VaultWhitelistDiscriminator {
     Config = 1,
 }
 
 impl Discriminator for Config {
-    const DISCRIMINATOR: u8 = HelloWorldNcnDiscriminator::Config as u8;
+    const DISCRIMINATOR: u8 = VaultWhitelistDiscriminator::Config as u8;
 }

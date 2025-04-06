535feb98c04d775f393ee3e568f174d26438fb5e
aoikurokawa <ht.so.ik0102@gmail.com>
2025-04-06 14:26:15 +0900

    fix: update

---
+++ vault-whitelist-core/src/discriminator.rs
@@ -0,0 +1,13 @@
+use jito_bytemuck::Discriminator;
+
+use crate::config::Config;
+
+/// Discriminators for HelloWorldNcn accounts
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum VaultWhitelistDiscriminator {
+    Config = 1,
+}
+
+impl Discriminator for Config {
+    const DISCRIMINATOR: u8 = VaultWhitelistDiscriminator::Config as u8;
+}

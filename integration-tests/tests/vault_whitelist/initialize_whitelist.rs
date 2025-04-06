5da6176cc3d193b50e7109a559c5261cf2d08bc3
aoikurokawa <ht.so.ik0102@gmail.com>
2025-04-07 07:35:33 +0900

    feat: initialize vault

--- integration-tests/tests/vault_whitelist/initialize_whitelist.rs
+++ integration-tests/tests/vault_whitelist/initialize_whitelist.rs
@@ -10,6 +10,11 @@ mod tests {
         let mut vault_program_client = fixture.vault_program_client();
         vault_program_client.do_initialize_config().await.unwrap();
 
+        vault_program_client
+            .do_initialize_vault(1000, 1000, 1000, 9, &Pubkey::new_unique(), None)
+            .await
+            .unwrap();
+
         let mut vault_whitelist_client = fixture.vault_whitelist_program_client();
         vault_whitelist_client.do_initialize_config().await.unwrap();
 

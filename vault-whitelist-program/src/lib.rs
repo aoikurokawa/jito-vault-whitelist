34c0dcf64a50174fa00a86d7a8f678c4b0958627
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2025-04-06 14:51:01 +0900

    Program (#11)
    * fix: update
    
    * fix: update
    
    * fix: update
    
    * fix: update

---
+++ vault-whitelist-program/src/lib.rs
@@ -0,0 +1,24 @@
+use const_str_to_pubkey::str_to_pubkey;
+use solana_program::{
+    account_info::AccountInfo, declare_id, entrypoint::ProgramResult, msg,
+    program_error::ProgramError, pubkey::Pubkey,
+};
+
+declare_id!(str_to_pubkey(env!("VAULT_WHITELIST_PROGRAM_ID")));
+
+#[cfg(not(feature = "no-entrypoint"))]
+solana_program::entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    if *program_id != id() {
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    msg!("Hello World");
+
+    Ok(())
+}

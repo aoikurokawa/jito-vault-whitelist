95f0263161e748555298fe851216bccbfc6600f0
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2025-04-07 06:56:19 +0900

    Initialize Vault (#23)
    * fix: update
    
    * fix: update

--- integration-tests/tests/fixtures/fixture.rs
+++ integration-tests/tests/fixtures/fixture.rs
@@ -2,7 +2,9 @@ use std::fmt::{Debug, Formatter};
 
 use solana_program_test::{processor, ProgramTest, ProgramTestContext};
 
-use crate::client::vault_whitelist_client::VaultWhitelistClient;
+use crate::client::{
+    vault_client::VaultProgramClient, vault_whitelist_client::VaultWhitelistClient,
+};
 
 pub struct TestBuilder {
     pub context: ProgramTestContext,
@@ -17,13 +19,23 @@ impl Debug for TestBuilder {
 impl TestBuilder {
     pub async fn new() -> Self {
         // $ cargo-build-sbf && SBF_OUT_DIR=$(pwd)/target/sbf-solana-solana/release cargo nextest run
-        let program_test = ProgramTest::new(
+        let mut program_test = ProgramTest::new(
             "jito_vault_whitelist_program",
             jito_vault_whitelist_program::id(),
             processor!(jito_vault_whitelist_program::process_instruction),
         );
+        program_test.prefer_bpf(true);
+        program_test.add_program("jito_vault_program", jito_vault_program::id(), None);
+
         let context = program_test.start_with_context().await;
         Self { context }
+    }
+
+    pub fn vault_program_client(&self) -> VaultProgramClient {
+        VaultProgramClient::new(
+            self.context.banks_client.clone(),
+            self.context.payer.insecure_clone(),
+        )
     }
 
     pub fn vault_whitelist_program_client(&self) -> VaultWhitelistClient {
--- integration-tests/tests/vault_whitelist/initialize_whitelist.rs
+++ integration-tests/tests/vault_whitelist/initialize_whitelist.rs
@@ -7,8 +7,10 @@ mod tests {
     #[tokio::test]
     async fn test_initialize_whitelist() {
         let fixture = TestBuilder::new().await;
-        let mut vault_whitelist_client = fixture.vault_whitelist_program_client();
+        let mut vault_program_client = fixture.vault_program_client();
+        vault_program_client.do_initialize_config().await.unwrap();
 
+        let mut vault_whitelist_client = fixture.vault_whitelist_program_client();
         vault_whitelist_client.do_initialize_config().await.unwrap();
 
         let vault = Pubkey::new_unique();

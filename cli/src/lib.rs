260e50b98e7e3803cbd209a5e42ff2442f75f0be
aoikurokawa <ht.so.ik0102@gmail.com>
2025-04-20 20:20:16 +0900

    fix: lint

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,17 +1,17 @@
 use ::log::info;
 use anyhow::anyhow;
-use base64::{engine::general_purpose, Engine};
+// use base64::{engine::general_purpose, Engine};
 use borsh::BorshDeserialize;
 use cli_config::CliConfig;
 use cli_signer::CliSigner;
 use jito_vault_whitelist_client::pretty_display::PrettyDisplay;
 use log::print_base58_tx;
-use solana_account_decoder::{UiAccountEncoding, UiDataSliceConfig};
+// use solana_account_decoder::{UiAccountEncoding, UiDataSliceConfig};
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
-use solana_rpc_client_api::{
-    config::{RpcAccountInfoConfig, RpcProgramAccountsConfig},
-    filter::{Memcmp, MemcmpEncodedBytes, RpcFilterType},
-};
+// use solana_rpc_client_api::{
+//     config::{RpcAccountInfoConfig, RpcProgramAccountsConfig},
+//     filter::{Memcmp, MemcmpEncodedBytes, RpcFilterType},
+// };
 use solana_sdk::{
     instruction::Instruction, pubkey::Pubkey, signers::Signers, transaction::Transaction,
 };
@@ -47,57 +47,58 @@ pub(crate) trait CliHandler {
         )
     }
 
-    /// Creates an RPC program accounts configuration for fetching accounts of type `T` with an optional public key filter.
-    ///
-    /// This method constructs a configuration that can be used with RPC methods to fetch program accounts
-    /// that match specific criteria. It automatically adds filters for the account data size and the discriminator
-    /// of type `T` to ensure only accounts of the expected type are returned.
-    fn get_rpc_program_accounts_config<T: jito_bytemuck::Discriminator>(
-        &self,
-        filter_pubkey: Option<(&Pubkey, usize)>,
-    ) -> anyhow::Result<RpcProgramAccountsConfig> {
-        let data_size = std::mem::size_of::<T>()
-            .checked_add(8)
-            .ok_or_else(|| anyhow!("Failed to add"))?;
+    // Creates an RPC program accounts configuration for fetching accounts of type `T` with an optional public key filter.
+    //
+    // This method constructs a configuration that can be used with RPC methods to fetch program accounts
+    // that match specific criteria. It automatically adds filters for the account data size and the discriminator
+    // of type `T` to ensure only accounts of the expected type are returned.
+    // fn get_rpc_program_accounts_config<T: jito_bytemuck::Discriminator>(
+    //     &self,
+    //     filter_pubkey: Option<(&Pubkey, usize)>,
+    // ) -> anyhow::Result<RpcProgramAccountsConfig> {
+    //     let data_size = std::mem::size_of::<T>()
+    //         .checked_add(8)
+    //         .ok_or_else(|| anyhow!("Failed to add"))?;
 
-        let encoded_discriminator =
-            general_purpose::STANDARD.encode(vec![T::DISCRIMINATOR, 0, 0, 0, 0, 0, 0, 0]);
-        let discriminator_filter = RpcFilterType::Memcmp(Memcmp::new(
-            0,
-            MemcmpEncodedBytes::Base64(encoded_discriminator),
-        ));
+    //     let encoded_discriminator =
+    //         general_purpose::STANDARD.encode(vec![T::DISCRIMINATOR, 0, 0, 0, 0, 0, 0, 0]);
+    //     let discriminator_filter = RpcFilterType::Memcmp(Memcmp::new(
+    //         0,
+    //         MemcmpEncodedBytes::Base64(encoded_discriminator),
+    //     ));
 
-        let mut filters = vec![
-            RpcFilterType::DataSize(data_size as u64),
-            discriminator_filter,
-        ];
+    //     let mut filters = vec![
+    //         RpcFilterType::DataSize(data_size as u64),
+    //         discriminator_filter,
+    //     ];
 
-        if let Some((pubkey, offset)) = filter_pubkey {
-            let pubkey_filter = RpcFilterType::Memcmp(Memcmp::new(
-                offset,
-                MemcmpEncodedBytes::Base64(general_purpose::STANDARD.encode(pubkey.to_bytes())),
-            ));
+    //     if let Some((pubkey, offset)) = filter_pubkey {
+    //         let pubkey_filter = RpcFilterType::Memcmp(Memcmp::new(
+    //             offset,
+    //             MemcmpEncodedBytes::Base64(general_purpose::STANDARD.encode(pubkey.to_bytes())),
+    //         ));
 
-            filters.push(pubkey_filter);
-        }
+    //         filters.push(pubkey_filter);
+    //     }
 
-        let config = RpcProgramAccountsConfig {
-            filters: Some(filters),
-            account_config: RpcAccountInfoConfig {
-                encoding: Some(UiAccountEncoding::Base64),
-                data_slice: Some(UiDataSliceConfig {
-                    offset: 0,
-                    length: data_size,
-                }),
-                commitment: None,
-                min_context_slot: None,
-            },
-            with_context: Some(false),
-            sort_results: Some(false),
-        };
+    //     let config = RpcProgramAccountsConfig {
+    //         filters: Some(filters),
+    //         account_config: RpcAccountInfoConfig {
+    //             encoding: Some(UiAccountEncoding::Base64),
+    //             data_slice: Some(UiDataSliceConfig {
+    //                 offset: 0,
+    //                 length: data_size,
+    //             }),
+    //             commitment: None,
+    //             min_context_slot: None,
+    //         },
+    //         with_context: Some(false),
+    //         sort_results: Some(false),
+    //     };
 
-        Ok(config)
-    }
+    //     Ok(config)
+    // }
+
     /// Fetches and deserializes an account
     ///
     /// This method retrieves account data using the configured RPC client,

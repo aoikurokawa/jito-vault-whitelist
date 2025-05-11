5823bd63955650a66af236340309fefb0b96feb6
aoikurokawa <ht.so.ik0102@gmail.com>
2025-05-11 14:12:25 +0900

    fix: remove error word

--- integration-tests/tests/fixtures/mod.rs
+++ integration-tests/tests/fixtures/mod.rs
@@ -10,10 +10,10 @@ pub type TestResult<T> = Result<T, TestError>;
 #[derive(Error, Debug)]
 pub enum TestError {
     #[error(transparent)]
-    BanksClientError(#[from] BanksClientError),
+    BanksClient(#[from] BanksClientError),
 
     #[error(transparent)]
-    ProgramError(#[from] ProgramError),
+    Program(#[from] ProgramError),
 
     /// Account not found
     #[error("Account not found")]
@@ -23,12 +23,12 @@ pub enum TestError {
 impl TestError {
     pub fn to_transaction_error(&self) -> Option<TransactionError> {
         match self {
-            TestError::BanksClientError(e) => match e {
+            TestError::BanksClient(e) => match e {
                 BanksClientError::TransactionError(e) => Some(e.clone()),
                 BanksClientError::SimulationError { err, .. } => Some(err.clone()),
                 _ => None,
             },
-            TestError::ProgramError(_) => None,
+            TestError::Program(_) => None,
             TestError::AccountNotFound => None,
         }
     }
